5b16b37e398ce4b9e99db65e35abb7d7931fff1f
IsaacMarquez907 <58242992+IsaacMarquez907@users.noreply.github.com>
2019-12-03 14:48:52 -0800

    Initial commit of repo.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// TODO: Add your database configuration to this file.
+// NOTE: If you are using source code control (like GitHub), make sure you 
+//       do not check in passwords!
+//
+module.exports = {
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_marquezi',
+  password: '5476',
+  database: 'cs340_marquezi'
+};
---
+++ src/index.js
@@ -0,0 +1,121 @@
+/**
+ * Node.js Web Application Template
+ * 
+ * The code below serves as a starting point for anyone wanting to build a
+ * website using Node.js, Express, Handlebars, and MySQL. You can also use
+ * Forever to run your service as a background process.
+ */
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+const path = require('path');
+
+const app = express();
+
+// Configure handlebars
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: '.hbs'
+});
+
+// Configure the views
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(path.basename(__dirname), 'views'));
+
+// Setup static content serving
+app.use(express.static(path.join(path.basename(__dirname), 'public')));
+
+
+
+
+
+
+
+/**
+ * Create a database connection. This is our middleware function that will
+ * initialize a new connection to our MySQL database on every request.
+ */
+const config = require('./config');
+function connectDb(req, res, next) {
+  console.log('Connecting to the database');
+  let connection = mysql.createConnection(config);
+  connection.connect();
+  req.db = connection;
+  console.log('Database connected');
+  next();
+}
+
+/**
+ * This is the handler for our main page. The middleware pipeline includes
+ * our custom `connectDb()` function that creates our database connection and
+ * exposes it as `req.db`.
+ */
+app.get('/', connectDb, function(req, res) {
+  console.log('Got request for the home page');
+
+  res.render('home');
+
+  close(req);
+});
+
+
+app.get("/home", (req, res) => {
+
+	res.render("home");
+
+});
+
+
+app.get("/Terms", (req, res) => {
+
+        res.render("Terms");
+
+});
+
+app.get("/Classes", (req, res) => {
+
+        res.render("Classes");
+
+});
+app.get("/ratings", (req, res) => {
+
+        res.render("ratings");
+
+});
+
+
+app.get("/login", (req, res) => {
+
+	res.render("login");
+
+});
+
+
+
+/**
+ * Handle all of the resources we need to clean up. In this case, we just need 
+ * to close the database connection.
+ * 
+ * @param {Express.Request} req the request object passed to our middleware
+ */
+function close(req) {
+  if (req.db) {
+    req.db.end();
+    req.db = undefined;
+    console.log('Database connection closed');
+  }
+}
+
+/**
+ * Capture the port configuration for the server. We use the PORT environment
+ * variable's value, but if it is not set, we will default to port 3000.
+ */
+const port = process.env.PORT || 3000;
+
+/**
+ * Start the server.
+ */
+app.listen(port, function() {
+  console.log('== Server is listening on port', port);
+});
